12af22be751b40b901a140043b7d955314957e09
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-08 15:17:54 -0800

    created some helper functions and started with sts auth

--- main.go
+++ main.go
@@ -17,51 +17,71 @@ import (
 
 func prettyPrint(unformattedJSON string) string {
 	formattedBytes, err := json.MarshalIndent(unformattedJSON, "", "  ")
-	if err {
+	if err != nil {
 		return ""
 	}
 
 	return string(formattedBytes)
 }
 
-func main() {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+func checkEnv() {
+	envVars := [3]string{
+		"BUCKET",
+		"REGION",
+		"SERVICE_ROLE_ARN",
+	}
 
-	sess, err := session.NewSession()
-	if err != nil {
-			fmt.Println("Error creating session ", err)
+	for _, v := range envVars {
+		_, ok := os.LookupEnv(v)
+		if !ok {
+			fmt.Println(fmt.Printf("No value found for %s in the .env file.", v))
+			os.Exit(1)
 			return
-	}
-	svc := s3.New(sess, aws.NewConfig().WithRegion("us-west-2"))
-
-	bucket, ok := os.LookupEnv("BUCKET_NAME")
-	if !ok {
-		fmt.Println("No bucket name found in the BUCKET_NAME env variable. Exiting.")
-		os.Exit(1)
-		return
-	}
-	input := &s3.ListObjectsV2Input{
-    Bucket:  aws.String(bucket),
-    MaxKeys: aws.Int64(2),
-	}
-
-	result, err := svc.ListObjectsV2(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case s3.ErrCodeNoSuchBucket:
-				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
 		}
 	}
+}
 
-	fmt.Println(prettyPrint(result))
+func getAWSSession(options session.Options) *session.Session {
+	return session.Must(session.NewSessionWithOptions(options))
+}
+
+func getSTSClient(sesh *session.Session, region string) *sts.STS {
+	return sts.New(sesh, aws.NewConfig().WithRegion(region))
+}
+
+func getS3Client(sesh *session.Session, region string) *s3.S3 {
+	return s3.New(sesh, aws.NewConfig().WithRegion(region))
+}
+
+func main() {
+	checkEnv()
+
+	regionFromEnv, _ := os.LookupEnv("REGION")
+	ownAccountSesh := getAWSSession(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	stsClient := getSTSClient(ownAccountSesh, regionFromEnv)
+
+	// input := &s3.ListObjectsV2Input{
+	// 	Bucket:  aws.String(bucket),
+	// 	MaxKeys: aws.Int64(2),
+	// }
+
+	// result, err := svc.ListObjectsV2(input)
+	// if err != nil {
+	// 	if aerr, ok := err.(awserr.Error); ok {
+	// 		switch aerr.Code() {
+	// 		case s3.ErrCodeNoSuchBucket:
+	// 			fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+	// 		default:
+	// 			fmt.Println(aerr.Error())
+	// 		}
+	// 	} else {
+	// 		// Print the error, cast err to awserr.Error to get the Code and
+	// 		// Message from an error.
+	// 		fmt.Println(err.Error())
+	// 	}
+	// }
+
+	// fmt.Println(prettyPrint(result))
 }
