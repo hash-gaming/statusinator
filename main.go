f5754f561e6557d31563d0d09853f81367d226c9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-08 15:11:07 -0800

    added some more services that we need

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"encoding/json"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/sts"
 
 	_ "github.com/joho/godotenv/autoload"
 )
