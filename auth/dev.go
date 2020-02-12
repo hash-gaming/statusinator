c550315c89e0cb5c46ed6b445d8ef904281f82ba
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-11 22:13:26 -0800

    added a function to get auth when in development mode

---
+++ auth/dev.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+
+	"github.com/YashdalfTheGray/statusinator/env"
+	"github.com/YashdalfTheGray/statusinator/util"
+)
+
+// GetDevAuth returns the credentials for an authenticated session using AWS STS
+func GetDevAuth(roleArn string) (*sts.AssumeRoleOutput, error) {
+	roleSessionName, err := util.GetSessionName(roleArn)
+	if err != nil {
+		return nil, err
+	}
+
+	ownAccountSesh := util.GetAWSSession(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+
+	stsClient := util.GetSTSClient(ownAccountSesh, env.Get(env.Region))
+
+	serviceAssumeRoleInput := &sts.AssumeRoleInput{
+		RoleArn:         &roleArn,
+		RoleSessionName: &roleSessionName,
+	}
+
+	return stsClient.AssumeRole(serviceAssumeRoleInput)
+}
