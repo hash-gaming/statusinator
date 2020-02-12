f8598af63051e5d2a20f3557d3b96f45d29f1d04
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-02-11 22:13:02 -0800

    added check for isdevelopment and new runtime env variable

--- env/check.go
+++ env/check.go
@@ -3,16 +3,23 @@ package env
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Check ensures that the necessary environment variables are present
 func Check() {
-	for _, v := range allEnvKeys {
-		_, ok := os.LookupEnv(v)
+	for _, key := range allEnvKeys {
+		value, ok := os.LookupEnv(key)
 		if !ok {
-			fmt.Println(fmt.Printf("No value found for %s in the .env file.", v))
+			fmt.Println(fmt.Printf("No value found for %s in the .env file.", key))
 			os.Exit(1)
-			return
+		}
+
+		if key == RuntimeEnv {
+			if strings.ToLower(value) != RuntimeDev && strings.ToLower(value) != RuntimeProd {
+				fmt.Println("ENV can only be set to either 'development' or 'production'.")
+				os.Exit(1)
+			}
 		}
 	}
 }
